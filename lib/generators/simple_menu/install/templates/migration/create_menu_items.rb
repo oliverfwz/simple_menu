355ea780b306a41ac5cd9a6be4c590f519e77599
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-09 15:01:01 +0700

    Add more sort_tree in generate create_menu_items

--- lib/generators/simple_menu/install/templates/migration/create_menu_items.rb
+++ lib/generators/simple_menu/install/templates/migration/create_menu_items.rb
@@ -1,15 +1,16 @@
 class CreateMenuItems < ActiveRecord::Migration
   def up
     create_table :menu_items do |t|
-      t.string    "title"
+      t.string     "title"
       t.string     "link"
       t.string     "page"
       t.string     "target", default: "_self"  
       t.integer    "ordering", default: 1
-      t.string    "show"
+      t.string     "show"
       t.integer    "level", default: 1
       t.boolean    "internal_link", default: true
-      t.boolean   "published", default: true
+      t.boolean    "published", default: true
+      t.string     "sort_tree"
       t.timestamps
       t.references :menu
     end
