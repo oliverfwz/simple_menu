c5837e4e70877e95668754cb83f9f9e5dd22178d
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-09 14:28:26 +0700

    Add #set_level to menu_item model

--- lib/generators/simple_menu/install/templates/models/menu_item.rb
+++ lib/generators/simple_menu/install/templates/models/menu_item.rb
@@ -8,4 +8,10 @@ class MenuItem < ActiveRecord::Base
   belongs_to :menu
   belongs_to :parent, class_name: "MenuItem", foreign_key: "menu_item_id"
   has_many   :children, class_name: "MenuItem"
+
+  before_save :set_level
+
+  def set_level
+    self.level = self.parent.present? ? self.parent.level + 1 : 1
+  end
 end
