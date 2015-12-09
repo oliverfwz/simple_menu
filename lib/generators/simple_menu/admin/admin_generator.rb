b0c838b2c9288d8121269a80ea6008edfbd354fd
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-09 17:18:21 +0700

    Add admin_generator

---
+++ lib/generators/simple_menu/admin/admin_generator.rb
@@ -0,0 +1,21 @@
+require 'rails/generators/migration'
+require 'bundler'
+
+module SimpleMenu
+  module Generators
+    class AdminGenerator < ::Rails::Generators::NamedBase
+      include Rails::Generators::Migration
+      source_root File.expand_path('../templates', __FILE__)
+      desc "add the migrations"
+
+      def copy_initializer_file
+        active = Bundler.load.specs.map { |spec| spec.name }
+        if active.include? "activeadmin"
+          template "activeadmin/menu.rb", "app/admin/menu.rb"
+          template "activeadmin/menu_item.rb", "app/admin/menu_item.rb"
+          template "activeadmin/position.rb", "app/admin/position.rb"
+        end
+      end
+    end
+  end
+end
